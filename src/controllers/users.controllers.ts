ec6fd53e4968191dba2ac436dd2fa6979649f616
ttvu <thanhvu6a1@gmail.com>
2023-10-09 10:48:30 +0700

    adjust login func

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -14,7 +14,6 @@ export const loginController = async (req: Request<ParamsDictionary, any, LoginR
   })
 }
 
-// date_of_birth: iso_string
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   const result = await userService.register(req.body)
   res.status(200).json({
