df8d589726cc77cde59cc10d3d1a19d828d03f59
Vux142857 <thanhvu6a1@gmail.com>
2024-04-25 13:17:13 +0700

    fix: Message

--- src/index.ts
+++ src/index.ts
@@ -47,68 +47,78 @@ io.use(
 )
 
 io.on('connection', async (socket) => {
-  const userID = socket.handshake.auth.id
-  const username = socket.handshake.auth.username
-  sessionStore.saveSession(userID, {
-    userID,
-    username: username,
-    socketID: socket.id,
-  })
-  const users: UserInConversation[] = []
-  let conversations = await redisService.getCachedConversationById(userID)
-  if (conversations.length < 1) {
-    conversations = await conversationService.getConversationsByUserId(new ObjectId(userID))
-    if (conversations.length > 0) {
-      await Promise.all(conversations.map(async (conversation: any) => {
-        await redisService.cacheConversationById(userID, conversation);
-      }));
+  try {
+    const userID = socket.handshake.auth.id
+    const username = socket.handshake.auth.username
+    sessionStore.saveSession(userID, {
+      userID,
+      username: username,
+      socketID: socket.id,
+    })
+    const users: UserInConversation[] = []
+    let conversations = await redisService.getCachedConversationById(userID)
+    if (conversations && conversations.length < 1) {
+      conversations = await conversationService.getConversationsByUserId(new ObjectId(userID))
+      if (conversations.length > 0) {
+        await Promise.all(conversations.map(async (conversation: any) => {
+          await redisService.cacheConversationById(userID, conversation);
+        }));
+      }
     }
-  }
-  conversations.map((item: any) => {
-    if (item.sender._id.toString() === userID) {
-      const isOnline = sessionStore.findSession(item.receiver._id.toString()) ? true : false
-      users.push({
-        userID: item.receiver._id.toString(),
-        name: item.receiver.name,
-        username: item.receiver.username,
-        avatar: item.receiver.avatar,
-        conversation: item._id.toString(),
-        isOnline
-      })
-    } else if (item.receiver._id.toString() === userID) {
-      const isOnline = sessionStore.findSession(item.sender._id.toString()) ? true : false
-      users.push({
-        userID: item.sender._id.toString(),
-        name: item.sender.name,
-        username: item.sender.username,
-        avatar: item.sender.avatar,
-        conversation: item._id.toString(),
-        isOnline
-      })
-    }
-  })
-  socket.emit('users', users)
 
-  socket.on('private message', async ({ content, from, to, conversation_id }) => {
-    const message: MessageConstructor = {
-      from,
-      content,
-      to,
-      conversation_id: new ObjectId(conversation_id)
+    if (conversations && conversations.length > 0) {
+      conversations.map((item: any) => {
+        if (item && item.sender && item.receiver) {
+          if (item.sender._id.toString() === userID) {
+            const isOnline = sessionStore.findSession(item.receiver._id.toString()) ? true : false
+            users.push({
+              userID: item.receiver._id.toString(),
+              name: item.receiver.name,
+              username: item.receiver.username,
+              avatar: item.receiver.avatar,
+              conversation: item._id.toString(),
+              isOnline
+            })
+          } else if (item.receiver._id.toString() === userID) {
+            const isOnline = sessionStore.findSession(item.sender._id.toString()) ? true : false
+            users.push({
+              userID: item.sender._id.toString(),
+              name: item.sender.name,
+              username: item.sender.username,
+              avatar: item.sender.avatar,
+              conversation: item._id.toString(),
+              isOnline
+            })
+          }
+        }
+      })
     }
-    const toUser = sessionStore.findSession(to)
-    if (toUser) {
-      socket.to(toUser.socketID).emit('receive message', message)
-    }
-    await Promise.all([
-      messageService.storeMessage(message),
-      redisService.cacheMessagesById(conversation_id, message)
-    ])
-  })
-  socket.on('disconnect', async () => {
-    sessionStore.deleteSession(userID)
-    console.log('User disconnected: ' + userID)
-  })
+
+    socket.emit('users', users)
+
+    socket.on('private message', async ({ content, from, to, conversation_id }) => {
+      const message: MessageConstructor = {
+        from,
+        content,
+        to,
+        conversation_id: new ObjectId(conversation_id)
+      }
+      const toUser = sessionStore.findSession(to)
+      if (toUser) {
+        socket.to(toUser.socketID).emit('receive message', message)
+      }
+      await Promise.all([
+        messageService.storeMessage(message),
+        redisService.cacheMessagesById(conversation_id, message)
+      ])
+    })
+    socket.on('disconnect', async () => {
+      sessionStore.deleteSession(userID)
+      console.log('User disconnected: ' + userID)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 server.listen(port, () => {
   console.log(`App listening on port ${port}`)
